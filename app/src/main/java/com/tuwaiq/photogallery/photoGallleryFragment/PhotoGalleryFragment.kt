bf4c47515cc18825ae21d7315fb189a757fd00dc
anasalqassim <anas.alqassim@gmail.com>
2021-11-21 15:51:49 +0300

    rv

--- app/src/main/java/com/tuwaiq/photogallery/photoGallleryFragment/PhotoGalleryFragment.kt
+++ app/src/main/java/com/tuwaiq/photogallery/photoGallleryFragment/PhotoGalleryFragment.kt
@@ -7,20 +7,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.LiveData
+import android.widget.ImageView
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tuwaiq.photogallery.R
-import com.tuwaiq.photogallery.flickr.api.FlickrApi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import com.tuwaiq.photogallery.flickr.models.GalleryItem
 
 private const val TAG = "PhotoGalleryFragment"
 class PhotoGalleryFragment : Fragment() {
 
+    private lateinit var photoGalleryRV:RecyclerView
 
     private val viewModel by lazy { ViewModelProvider(this)[PhotoGalleryViewModel::class.java] }
 
@@ -45,7 +43,10 @@ class PhotoGalleryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.photo_gallery_fragment, container, false)
+        val view =  inflater.inflate(R.layout.photo_gallery_fragment, container, false)
+        photoGalleryRV = view.findViewById(R.id.photoGallery_rv)
+        photoGalleryRV.layoutManager = GridLayoutManager(context,3)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -53,4 +54,32 @@ class PhotoGalleryFragment : Fragment() {
 
     }
 
+    private inner class GalleryHolder(val view: View):
+            RecyclerView.ViewHolder(view){
+                private val photoItem:ImageView = itemView.findViewById(R.id.photo_item)
+
+           fun bind(galleryItem: GalleryItem){
+
+           }
+
+
+            }
+    private inner class GalleryAdapter(val galleryItems:List<GalleryItem>):
+            RecyclerView.Adapter<GalleryHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryHolder {
+            val view = layoutInflater.inflate(
+                R.layout.photo_list_item,
+                parent,
+                false
+            )
+            return GalleryHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: GalleryHolder, position: Int) {
+            val galleryItem = galleryItems[position]
+            holder.bind(galleryItem)
+        }
+
+        override fun getItemCount(): Int = galleryItems.size
+    }
 }
